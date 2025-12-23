93419aa7e7565a96ea35944292724ebee59bd9f8
Louis Mamon <Louis M>
2025-12-22 15:00:37 -0900

    Initial robo-sandbox structure

---
+++ rosgz/autopilot_mode.zsh
@@ -0,0 +1,7 @@
+export COAST_WATER_SHADER_PLUGIN=gz-sim-shader-param-system
+export COAST_WATER_SHADER_NAME=gz::sim::systems::ShaderParam
+
+export NAVSAT_PLUGIN=gz-sim-navsat-system
+export NAVSAT_NAME=gz::sim::systems::NavSat
+
+echo "PX4 mode (Gazebo Harmonic) active"

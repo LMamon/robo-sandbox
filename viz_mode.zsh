8e171b25eaee1659730eeecf75a7c6e08a8f0032
Louis Mamon <Louis M>
2026-02-23 23:10:05 -0900

    chore: elevating .zsh files for cleaner sourcing

---
+++ viz_mode.zsh
@@ -0,0 +1,7 @@
+export COAST_WATER_SHADER_PLUGIN=ignition-gazebo-shader-param-system
+export COAST_WATER_SHADER_NAME=ignition::gazebo::systems::ShaderParam
+
+export NAVSAT_PLUGIN=ignition-gazebo-navsat-system
+export NAVSAT_NAME=ignition::gazebo::systems::NavSat
+
+echo "Visualization mode (Ignition / Fortress) active"

4e391cf3e94cf1408364f7a291c7ea2b12ae769f
Louis Mamon <Louis M>
2026-04-02 23:01:57 -0800

    feat: externalize ov configs

--- rosgz/src/rxsim/setup.py
+++ rosgz/src/rxsim/setup.py
@@ -11,6 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'config', 'openvins'),
+            glob('config/openvins/*.yaml')),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'),
             glob('launch/*launch.py')),

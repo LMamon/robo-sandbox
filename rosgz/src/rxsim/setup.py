088d7d319c3cd7271da83553ff74a65dbfd51760
Louis Mamon <Louis M>
2026-05-31 15:56:48 -0800

    build: updated details and packages

--- rosgz/src/rxsim/setup.py
+++ rosgz/src/rxsim/setup.py
@@ -6,24 +6,24 @@ package_name = 'rxsim'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        (os.path.join('share', package_name, 'config', 'openvins'),
-            glob('config/openvins/*.yaml')),
+        (os.path.join('share', package_name, 'config', 'nvblox'),
+            glob('config/nvblox/*.yaml')),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'),
             glob('launch/*launch.py')),
-        (os.path.join('share', package_name, 'launch'), 
-            glob('launch/*.yaml'))
+        (os.path.join('share', package_name, 'config'), 
+            glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Louis M',
     maintainer_email='maintainer@example.com',
-    description='integration package for VIO with autonomy pipeline.',
+    description='integration package for vSLAM + 3d reconstruction with autonomy pipeline.',
     license='Apache-2.0',
     extras_require={
         'test': [
